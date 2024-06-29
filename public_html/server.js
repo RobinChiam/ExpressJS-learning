d5e429ad346f536e739733e51f1bc52f2f43b82b
Robin Chiam <raresugarglider@gmail.com>
2024-06-29 23:24:08 +0800

    Running on Port 3000 able to access from localhost:3000

--- public_html/server.js
+++ public_html/server.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+
+     console.log("Running on Port 3000")
+     res.send("Hello World")
+
+})
+
+app.listen(3000)
